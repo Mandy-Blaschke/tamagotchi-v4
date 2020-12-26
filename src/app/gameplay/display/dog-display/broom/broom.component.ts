5a1873b127af5e5097293e25d108985ee879eb56
Mandy Blaschke <post@mandy-blaschke.de>
2020-12-26 23:07:48 +0100

    added: new components for dog pet

---
+++ src/app/gameplay/display/dog-display/broom/broom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-broom',
+  templateUrl: './broom.component.html',
+  styleUrls: ['./broom.component.scss']
+})
+export class BroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
